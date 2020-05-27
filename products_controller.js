efaed46dd9b5cce2fa24863d4ac3f1e5f771ea9c
bbdoggett3 <bbdoggett3@gmail.com>
2020-05-27 15:58:14 -0600

    datbase finised

--- products_controller.js
+++ products_controller.js
@@ -3,8 +3,9 @@ module.exports = {
     create: (req, res, next) => {
         //create_product.sql
         const db = req.app.get('db');
+        const {name, description, price, image_url} = req.body;
 
-        db.create_product()
+        db.create_product([name, description, price, image_url])
         .then(() => res.sendStatus(200))
         .catch(error => {
             res.status(500).send({errorMessage: 'Opps! Something went wrong'});
@@ -15,8 +16,9 @@ module.exports = {
     getOne: (req, res, next) => {
         //read_product.sql
         const db = req.app.get('db');
+        const {id} = req.params
 
-        db.read_product()
+        db.read_product(id)
         .then(product => res.status(200).send(product))
         .catch(error => {
             res.status(500).send({errorMessage: 'Opps! Something went wrong'});
@@ -39,8 +41,9 @@ module.exports = {
     update: (req, res, next) => {
         // update_product.sql
         const db = req.app.get('db');
+        const {params, query} = req;
 
-        db.update_product()
+        db.update_product(params.id, query.desc)
         .then(() => res.sendStatus(200))
         .catch(error => {
             res.status(500).send({errorMessage: `Opps! Something went wrong`});
@@ -51,8 +54,9 @@ module.exports = {
     delete: (req, res, next) => {
         // delete_product.sql
         const db = req.app.get('db');
+        const {id} = req.params
 
-        db.delete()
+        db.delete(id)
         .then(() => sendStatus(200))
         .catch(error => {
             res.status(500).send({errorMessage: 'Opps! Something went wrong'});
